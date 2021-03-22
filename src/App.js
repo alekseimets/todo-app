d2bbaa1682797b8b171c19c9799115868c39bcf6
alekseimets <alekseimets@gmail.com>
2021-03-22 21:30:02 +0200

    added usestate key and set up on submit in form tag also on change in TextField tag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TextField from 'material-ui/core/TextField';
 
-function App() {
+const ToDoApp = ({saveToDo}) => {
+  const [value, setValue] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={(event) => {event.preventDefault(); saveToDo(value);}}>
+      <TextField variant="outlined" placeholder="Add todo" margin="normal" onChange={(event) => {setValue(event.target.value);}} value={value}/>
+    </form>
   );
-}
+};
 
-export default App;
+export default ToDoApp;
